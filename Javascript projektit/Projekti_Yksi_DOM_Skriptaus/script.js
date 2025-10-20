86cf535f18ea8bbcde99c71eaa60a8c51b071b96
QsfoxStudyHome <kalle.heiskanen@student.laurea.fi>
2025-10-20 04:20:20 +0300

    projekti 1.6

--- Javascript projektit/Projekti_Yksi_DOM_Skriptaus/script.js
+++ Javascript projektit/Projekti_Yksi_DOM_Skriptaus/script.js
@@ -1,57 +1,65 @@
 const lista = document.querySelector("#lista-ostokset");
 const kenttä = document.querySelector("#tavara");
 
+document
+  .getElementById("ostoslomake")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    lisaalistaan();
+  });
 
-document.getElementById("nappi").addEventListener("click", lisaalistaan);
 document.getElementById("poisto-nappi").addEventListener("click", listapoisto);
-
-document.getElementById("tyhjennä-nappi").addEventListener("click", function() {
-  lista.innerHTML = '';
-});
-
+document
+  .getElementById("tyhjennä-nappi")
+  .addEventListener("click", function () {
+    lista.innerHTML = "";
+  });
 
 function lisaalistaan() {
-
   const listateksti = kenttä.value.trim();
 
-  if (listateksti === '' || listateksti.length < 3) {
-        alert('Vähintään 3 merkkiä pitkä!');
-        kenttä.classList.add('virhe');
-        return;
+  if (listateksti === "" || listateksti.length < 3) {
+    alert("Vähintään 3 merkkiä pitkä!");
+    kenttä.className = "virhe";
+    return;
   }
 
-  kenttä.classList.remove('virhe');
+  kenttä.className = "";
 
   const listarivi = luodaanlistarivi(listateksti);
 
   lista.appendChild(listarivi);
 
-  kenttä.value = '' ;
+  kenttä.value = "";
 }
 
 function listapoisto() {
+  const listanviimeinen = lista.lastElementChild;
 
-    const listanviimeinen = lista.lastElementChild;
-
-    if (listanviimeinen) {
-
-        listanviimeinen.remove();
-    }
-
+  if (listanviimeinen) {
+    listanviimeinen.remove();
+  }
 }
 
 function luodaanlistarivi(teksti) {
-    const listarivi = document.createElement("li");
+  const listarivi = document.createElement("li");
 
-    listarivi.innerHTML = `${teksti} <input type="checkbox">`;
-    
-    const checkbox = listarivi.querySelector('input');
+  const tekstispan = document.createElement("span");
+  tekstispan.textContent = teksti;
 
-    checkbox.addEventListener('change', function() {
-        this.parentNode.classList.toggle('ostettu');
-    });
-    
-    return listarivi;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  checkbox.addEventListener("change", function () {
+    if (tekstispan.className === "ostettu") {
+      tekstispan.className = "";
+    } else {
+      tekstispan.className = "ostettu";
+    }
+  });
+
+  listarivi.appendChild(tekstispan);
+  listarivi.appendChild(checkbox);
+
+  return listarivi;
 }
-
-
